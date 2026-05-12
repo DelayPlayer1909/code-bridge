300c5610783efcc490714d45691bc670377ebba1
DelayPlayer1909 <cshekhar.p100@gmail.com>
2026-05-12 20:41:02 +0530

    great changess

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,18 +1,13 @@
 import SidebarButton from "@/components/sidebar/sidebar-views/SidebarButton"
-import { useAppContext } from "@/context/AppContext"
-import { useSocket } from "@/context/SocketContext"
+
+
 import { useViews } from "@/context/ViewContext"
 import useResponsive from "@/hooks/useResponsive"
-import useWindowDimensions from "@/hooks/useWindowDimensions"
-import { ACTIVITY_STATE } from "@/types/app"
-import { SocketEvent } from "@/types/socket"
+
+
 import { VIEWS } from "@/types/view"
-import { IoCodeSlash } from "react-icons/io5"
-import { MdOutlineDraw } from "react-icons/md"
+
 import cn from "classnames"
-import { Tooltip } from 'react-tooltip'
-import { useState } from 'react'
-import { buttonStyles, tooltipStyles } from "./tooltipStyles"
 
 function Sidebar() {
     const {
@@ -20,27 +15,11 @@ function Sidebar() {
         isSidebarOpen,
         viewComponents,
         viewIcons,
-        setIsSidebarOpen,
     } = useViews()
     const { minHeightReached } = useResponsive()
-    const { activityState, setActivityState } = useAppContext()
-    const { socket } = useSocket()
-    const { isMobile } = useWindowDimensions()
-    const [showTooltip, setShowTooltip] = useState(true)
 
-    const changeState = () => {
-        setShowTooltip(false)
-        if (activityState === ACTIVITY_STATE.CODING) {
-            setActivityState(ACTIVITY_STATE.DRAWING)
-            socket.emit(SocketEvent.REQUEST_DRAWING)
-        } else {
-            setActivityState(ACTIVITY_STATE.CODING)
-        }
+ 
 
-        if (isMobile) {
-            setIsSidebarOpen(false)
-        }
-    }
 
     return (
         <aside className="flex w-full md:h-full md:max-h-full md:min-h-full md:w-auto">
@@ -72,8 +51,6 @@ function Sidebar() {
                     viewName={VIEWS.SETTINGS}
                     icon={viewIcons[VIEWS.SETTINGS]}
                 />
-
-            e
             </div>
             <div
                 className="absolute left-0 top-0 z-20 w-full flex-col bg-dark/95 backdrop-blur-xl md:static md:min-w-[300px] md:border-r md:border-white/5"
